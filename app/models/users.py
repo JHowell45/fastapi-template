4c19cd5b67b960786289097d9d85fa1609b0088c
= <jhowell45@googlemail.com>
2025-06-24 23:53:54 +0100

    create user model to building the database user.

--- app/models/users.py
+++ app/models/users.py
@@ -1,14 +1,18 @@
-from pydantic import EmailStr
+from pydantic import EmailStr, SecretStr, computed_field
 from sqlalchemy_utils import StringEncryptedType
 from sqlalchemy_utils.types.encrypted.encrypted_type import AesEngine
 from sqlmodel import Column, Field, SQLModel, String
 
+from app.dependencies.auth import hash_secret
 from app.dependencies.config import get_settings
 
 
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class UserBase(SQLModel):
     email: EmailStr = Field(nullable=False)
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     hashed_password: str = Field(
         min_length=20,
         max_length=128,
@@ -20,3 +24,12 @@ class User(SQLModel, table=True):
             nullable=False,
         ),
     )
+
+
+class UserCreate(UserBase):
+    password: SecretStr
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def hashed_password(self) -> str:
+        return hash_secret(self.password.get_secret_value())
