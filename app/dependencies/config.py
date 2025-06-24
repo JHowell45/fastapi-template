e29c0cb262473651ac03ae614cb6df683e725fdf
= <jhowell45@googlemail.com>
2025-06-24 23:37:00 +0100

    creating the database connection correctly.

--- app/dependencies/config.py
+++ app/dependencies/config.py
@@ -1,6 +1,8 @@
 from typing import Annotated
 
 from fastapi import Depends
+from pydantic import PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -10,6 +12,22 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
+    DB_ENABLE_SSL: bool = True
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def DATABASE_URI(self) -> PostgresDsn:
+        path: str = self.DB_NAME
+        if self.DB_ENABLE_SSL:
+            path += "?sslmode=require"
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            path=path,
+        )
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=True
