c944e652b8fd990c4479eb9c21d3f25c41de5ecb
= <jhowell45@googlemail.com>
2025-06-24 23:28:02 +0100

    adding missing env vars.

--- app/dependencies/config.py
+++ app/dependencies/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_USER: str
     DB_PASSWORD: str
+    DB_NAME: str
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=True
