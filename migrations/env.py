7f507ad8b1e9a18eba8c587c66edb61884e00e62
= <jhowell45@googlemail.com>
2025-06-24 23:54:29 +0100

    Adding User model to be watched by migration autogen,

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from app.dependencies.config import get_settings
+from app.models.users import User  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
